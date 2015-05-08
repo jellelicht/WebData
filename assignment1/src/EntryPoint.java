b4dfce5a632c060ddd6750f942075e3bc5650a5e
Jelle Licht <wordempire@gmail.com>
2015-05-08 13:15:18 +0200

    [Assignment1] Fleshed out entry point with stack setup

--- assignment1/src/EntryPoint.java
+++ assignment1/src/EntryPoint.java
@@ -1,7 +1,14 @@
+import java.util.List;
+
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.SAXException;
+
+
 
 public class EntryPoint {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SAXException {
 		PatternNode root = new PatternNode("people", NodeType.ELEMENT, false).addChild( 
 				new PatternNode("person", NodeType.ELEMENT, false)
 				.addChild(new PatternNode("email", NodeType.ELEMENT, true))
@@ -9,5 +16,21 @@ public class EntryPoint {
 								.addChild(new PatternNode("last",
 										NodeType.ELEMENT, true))));
 		System.out.println("hello");
+		
+		XMLReader saxReader = 
+			    XMLReaderFactory.createXMLReader("org.apache.xerces.parsers.SAXParser");
+		saxReader.setContentHandler(new StackEval(generate(root, null)));
+	}
+	
+	public static TPEStack generate (PatternNode root, TPEStack parent){
+		if(root == null) {
+			throw new Error("Root cannot be null");
+		}
+		List<PatternNode> children = root.getChildren();
+		TPEStack ts = new TPEStack(root, parent);
+		for (PatternNode child : children){
+			ts.addChildStack(generate(child, ts));
+		}
+		return ts;
 	}
 }
